9156ed994e7e54d2c387213a04f7ef7552a14f3a
minh <nhatminh@MacBook-Pro-cua-minh.local>
2024-04-16 21:58:47 +0700

    Done create layout for user

---
+++ src/main/java/com/quanlyphongkhamvadatlich/controllers/user/HomeController.java
@@ -0,0 +1,24 @@
+package com.quanlyphongkhamvadatlich.controllers.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String index() {
+        return "user/pages/index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "user/pages/about";
+    }
+
+    @GetMapping("/procedure")
+    public String procedure() {
+        return "user/pages/procedure";
+    }
+}
