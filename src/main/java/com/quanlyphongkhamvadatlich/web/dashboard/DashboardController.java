d212990ac30165f2985e4904b5f8035170157d4e
Anhnaoto <50966044+Anhnato@users.noreply.github.com>
2024-04-24 15:33:36 +0700

    New change

--- src/main/java/com/quanlyphongkhamvadatlich/web/dashboard/DashboardController.java
+++ src/main/java/com/quanlyphongkhamvadatlich/web/dashboard/DashboardController.java
@@ -15,4 +15,20 @@ public class DashboardController {
 
         return new ModelAndView("doctor/physical_exam");
     }
+    @GetMapping("/admin/doctor")
+    public ModelAndView toDoctor() {
+        return new ModelAndView("admin/doctor");
+    }
+    @GetMapping("/admin/doctor_edit")
+    public ModelAndView toDoctorEdit() {
+        return new ModelAndView("admin/doctor_edit");
+    }
+    @GetMapping("/admin/patient")
+    public ModelAndView toPatient() {
+        return new ModelAndView("admin/patient");
+    }
+    @GetMapping("/admin/patient_edit")
+    public ModelAndView toPatientEdit() {
+        return new ModelAndView("admin/patient_edit");
+    }
 }
