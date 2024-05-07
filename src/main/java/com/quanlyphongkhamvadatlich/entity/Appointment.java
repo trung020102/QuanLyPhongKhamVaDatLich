aafd9309d849f05dd9fa5ae8e799cf7f843adb62
TRAN VAN HIEU <tranvanhieu02095501@gmail.com>
2024-05-07 16:45:26 +0700

    Be schedule appointment

---
+++ src/main/java/com/quanlyphongkhamvadatlich/entity/Appointment.java
@@ -0,0 +1,43 @@
+package com.quanlyphongkhamvadatlich.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "appointments")
+public class Appointment extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "appointment_date")
+    private Date appointmentDate;
+
+    @Column(name = "time_slot")
+    private String timeSlot;
+
+    @Column(name = "order_number")
+    private Integer orderNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appointment_status_id")
+    private AppointmentStatus appointmentStatus;
+}
